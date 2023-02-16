51ca27b643e26d8564740a37531e4a006ae611d0
morris andanje <morrisandanje@example.com>
2023-02-16 15:00:00 -0800

    alx-low_level_programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    //string initialisation
+    char Mystr1[50] = "Programming is like building a multilingual puzzle";
+
+    puts(Mystr1); //not specifically adding a new line
+
+    return 0;
+}
