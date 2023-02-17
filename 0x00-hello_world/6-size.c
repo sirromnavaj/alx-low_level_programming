42d569426fb68820a72344b8aece44fccf05594c
morris andanje <morrisandanje@example.com>
2023-02-16 18:35:16 -0800

    alx-low_level_programming

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * return: Always 0 (Success)
+*/
+int main(void
+{
+char c = 1;
+int d = 4;
+long int a = 4;
+long long int b = 8;
+float e = 4;
+printf("Size of a char: %c",c"byte(s)");
+printf("Size of an int: %d",d"byte(s)");
+printf("Size of a long int: %d",a"byte(s)");
+printf("Size of a long long int: %d",b"byte(s)");
+printf("Size of a float: %f",e"byte(s)");
+return (0);
+}
