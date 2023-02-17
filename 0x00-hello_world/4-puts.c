c0ef99944d8b706e5060c73b2f24f8e626b37db8
morris andanje <morrisandanje@example.com>
2023-02-16 16:43:30 -0800

    alx-low_level_programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,22 +1,15 @@
 #include <stdio.h>
 #include <string.h>
-
 /**
  * main - Entry point
  *
  *Description: 'Prints exactly \"programming is like building a multilingual puzzle'
- *
- *string initialisation
- *
- * writing to stdout
  *
  * return always 0 (Success)
 */
 int main(void)
 {	
 char str1[60] = "\"programming is like building a multilingual puzzle";
-
 puts(str1);
-
 return (0);
 }
